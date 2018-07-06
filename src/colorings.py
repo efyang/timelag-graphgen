403e87f45546f1d343cde0f7f12c6d7ce2c7b27b
efyang <edward.yang6771@gmail.com>
2018-07-06 12:06:23 -0500

    Add discrete coloring and start work on more paramterization

--- src/colorings.py
+++ src/colorings.py
@@ -1,11 +1,11 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib import colors
 import numpy as np
 
 
-def get_colors(n, cmap_id, seasonal=False):
-    cmap = plt.get_cmap(cmap_id)
+def get_colors_cmap_linear(n, cmap, seasonal=False):
     norm = mpl.colors.Normalize()
     m = cm.ScalarMappable(norm=norm, cmap=cmap)
     if seasonal:
@@ -16,3 +16,16 @@ def get_colors(n, cmap_id, seasonal=False):
     m.set_array(col)
 
     return m, m.to_rgba(col)
+
+
+def get_months(dates):
+    return dates.apply(lambda x: x[:x.index('/')]).apply(int)
+
+
+def discrete_colormap(months):
+    cmap = colors.ListedColormap(['b', 'g', 'r', 'y'])
+    bounds = [1, 3, 6, 9, 12]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+    m = cm.ScalarMappable(norm=norm, cmap=cmap)
+    m.set_array(months)
+    return m, m.to_rgba(months)
